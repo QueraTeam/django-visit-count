ce49b621107ccea3d0faf70546997efc5bf6b6c8
Mohammad Javad Naderi <mjnaderi@gmail.com>
2022-09-13 13:12:11 +0430

    Add abstract=True

--- django_visit_count/models.py
+++ django_visit_count/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-from django.db.models import F
 from django.utils.translation import gettext_lazy as _
 
 from .app_settings import VISIT_COUNT_DEFAULT_SESSION_DURATION
 from .utils import is_new_visit
 
 
-class VisitCountMixin:
+class VisitCountMixin(models.Model):
     visit_count = models.PositiveIntegerField(default=0, help_text=_("Visit count"))
+
+    class Meta:
+        abstract = True
 
     def count_visit(self, request, session_duration=VISIT_COUNT_DEFAULT_SESSION_DURATION):
         if is_new_visit(request, self, session_duration=session_duration):
-            self.visit_count = F("visit_count") + 1
+            self.visit_count = models.F("visit_count") + 1
             self.save(update_fields=["visit_count"])
